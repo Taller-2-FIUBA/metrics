441725510cfdd9ad7434d47240008d70a018de3a
MarianoCinalli <cinallimariano1992@gmail.com>
2023-06-25 00:09:21 -0300

    chore: add healthcheck

--- metrics/constants.py
+++ metrics/constants.py
@@ -1,8 +1,10 @@
 """Application constants."""
 
+import time
 from environ import to_config
 from metrics.config import AppConfig
 
 CONFIGURATION = to_config(AppConfig)
+START = time.time()
 BASE_URI = "/metrics"
 DOCUMENTATION_URI = BASE_URI + "/documentation/"
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,5 +1,6 @@
 # pylint: disable= missing-module-docstring, missing-function-docstring
 from fastapi.testclient import TestClient
+from hamcrest import assert_that, greater_than
 from metrics.main import app
 
 client = TestClient(app)
@@ -19,3 +20,9 @@ def test_when_getting_swagger_ui_expect_200():
 def test_when_getting_openapi_doc_expect_200():
     response = client.get("metrics/documentation/openapi.json")
     assert response.status_code == 200, response.json()
+
+
+def test_when_checking_healthcheck_expect_uptime_greater_than_zero():
+    response = client.get("metrics/healthcheck/")
+    assert response.status_code == 200, response.json()
+    assert_that(response.json()["uptime"], greater_than(0))
